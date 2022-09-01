ae8a8bd0447c25232c8288f88a33347363f3a517
Ananya <ananya1474.be20@chitkara.edu.in>
2022-09-01 23:04:33 +0530

    1st Sep

--- 25th Aug,22/1/react-task-tracker/src/Components/Header.js
+++ 25th Aug,22/1/react-task-tracker/src/Components/Header.js
@@ -1,9 +1,15 @@
-const Header= () => {
+const Header= (props) => {     //props=properties which means whatever written there will be linked with this page 
     return(
         <header>
-            <h1>Hello Header</h1>
+            <h1>Hello,{props.title}</h1> //or Hello,{title}
         </header>
     );
 
 };
+Header.defaultProps={
+    title:"Task Tracker",
+};
+Header.propTypes={
+    title:propTypes.dtring.isRequired,
+}
 export default Header;
